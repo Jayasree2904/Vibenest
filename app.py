96744b04c51e63dcb1ee404f687dc71671cb50b6
Jayasree2904 <jayasree0429@gmail.com>
2025-07-20 12:47:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -98,8 +98,6 @@ def emoji_recommendations():
             'message': str(e)
         }), 500
 
-import os
-
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
